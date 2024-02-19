c5ceb902947ce93272926b39ff4ad7754d960bba
simoncra <csimonr28@gmail.com>
2024-02-18 21:14:50 -0400

    data de prueba para inicializar la DB

--- data_prueba.sql
+++ data_prueba.sql
@@ -1,24 +1,22 @@
---usuario
-INSERT INTO profiles."user"
-(id_user, "name", email, "password", fk_id_cat_status, create_date, pwd_expiration_date, last_session, logical_delete)
-VALUES
-(1, 'jsalge', 'josesalge@gmail.com', '1234', 1, '2024-01-26 12:27:20.493', '2024-01-26 12:27:20.493', '2024-01-26 12:27:20.493', false);
+INSERT INTO public.cat_categories
+(id, created_at, updated_at, deleted_at, "name", description, logical_delete)
+VALUES(1, '2024-02-07 23:02:00.191', '2024-02-07 23:02:00.191', NULL, 'General', 'Categoria general', false);
 
---clave
-INSERT INTO transactions.task
-(id_task, fk_id_user, fk_id_cat_category, description, fk_id_cat_status, create_date, current_task_date, last_status_change, logical_delete)
-VALUES(1, 1, 1, 'prueba', 1, '2024-01-28 20:56:03.516', '2024-01-28 20:56:03.516', '2024-01-28 20:56:03.516', false);
-INSERT INTO transactions.task
-(id_task, fk_id_user, fk_id_cat_category, description, fk_id_cat_status, create_date, current_task_date, last_status_change, logical_delete)
-VALUES(2, 1, 1, 'prueba', 2, '2024-01-28 20:56:03.516', '2024-01-28 20:56:03.516', '2024-01-28 20:56:03.516', false);
-INSERT INTO transactions.task
-(id_task, fk_id_user, fk_id_cat_category, description, fk_id_cat_status, create_date, current_task_date, last_status_change, logical_delete)
-VALUES(3, 1, 1, 'prueba', 1, '2024-01-28 20:56:03.516', '2024-01-28 20:56:03.516', '2024-01-28 20:56:03.516', true);
-INSERT INTO transactions.task
-(id_task, fk_id_user, fk_id_cat_category, description, fk_id_cat_status, create_date, current_task_date, last_status_change, logical_delete)
-VALUES(4, 1, 1, 'prueba', 1, '2024-01-28 20:56:03.516', '2024-01-29 20:56:03.516', '2024-01-28 20:56:03.516', false);
-INSERT INTO transactions.task
-(id_task, fk_id_user, fk_id_cat_category, description, fk_id_cat_status, create_date, current_task_date, last_status_change, logical_delete)
-VALUES(5, 1, 1, 'prueba', 2, '2024-01-28 20:56:03.516', '2024-01-29 20:56:03.516', '2024-01-28 20:56:03.516', false);
 
-SELECT setval('transactions.task_seq',(select coalesce(max(id_task),0) from transactions.task), true);
+INSERT INTO public.cat_statuses
+(id, created_at, updated_at, deleted_at, "name", description, logical_delete)
+VALUES(1, '2024-02-07 23:08:41.723', '2024-02-07 23:08:41.723', NULL, 'Pendiente', 'Tarea pendiente', false);
+INSERT INTO public.cat_statuses
+(id, created_at, updated_at, deleted_at, "name", description, logical_delete)
+VALUES(2, '2024-02-07 23:08:41.723', '2024-02-07 23:08:41.723', NULL, 'Realizada', 'Tarea realizada', false);
+INSERT INTO public.cat_statuses
+(id, created_at, updated_at, deleted_at, "name", description, logical_delete)
+VALUES(3, '2024-02-07 23:08:41.723', '2024-02-07 23:08:41.723', NULL, 'Activo', 'Usuario activo', false);
+INSERT INTO public.cat_statuses
+(id, created_at, updated_at, deleted_at, "name", description, logical_delete)
+VALUES(4, '2024-02-07 23:08:41.723', '2024-02-07 23:08:41.723', NULL, 'Inactivo', 'Usuario inactivo', false);
+
+
+INSERT INTO public.users
+(id, created_at, updated_at, deleted_at, "name", email, "password", password_expiration_date, fk_id_cat_status, last_session, logical_delete)
+VALUES(4, '2024-02-07 23:16:40.012', '2024-02-07 23:16:40.012', NULL, 'simon', 'simoncra@gmail.com', '$2a$14$I8eVrIZipomAaKhjVQfVOOa71r/kc/MA.XtFtxjLRHl7U5B3Zxddu', '2024-02-07 23:16:40.009', 4, '2024-02-07 23:16:40.009', false);
